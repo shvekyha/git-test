30ea8af53cf1d6a60c50822c9db5e2f76ed3fc3f
Hadas Shveky Teman <shvekyha@gmail.com>
2021-09-18 12:34:24 +0300

    fixed bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello react on git - I have a bug
+          Hello react on git - fixed bug
         </p>
         <a
           className="App-link"
